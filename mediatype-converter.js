100207b5de8d8e67019a306531d5eb321ab0b8f8
Dennis Wolters <dbn@gmx.de>
2016-12-09 11:57:10 +0100

    Added JSDoc and fixed linting errors

--- mediatype-converter.js
+++ mediatype-converter.js
@@ -1,20 +1,26 @@
-var pandocToMediaType = require('./mediatype-mapping.json');
+const pandocToMediaType = require('./mediatype-mapping.json');
 
-var mediaTypeToPandoc = {};
-for(var key in pandocToMediaType) {
-  if(pandocToMediaType[key]) {
-    mediaTypeToPandoc[pandocToMediaType[key]] = key;
-  }
+let mediaTypeToPandoc = {};
+for (let key in pandocToMediaType) {
+    if (pandocToMediaType[key]) {
+        mediaTypeToPandoc[pandocToMediaType[key]] = key;
+    }
 }
 
-function convertType (type) {
-  if(mediaTypeToPandoc[type]) {
-    return mediaTypeToPandoc[type];
-  } else if(type.match(/^application\/x\./)) {
-    return type.substr(type.indexOf('.')+1);
-  } else {
-    return type;
-  }
+/**
+ * Converts a media type into a pandoc type identifier.
+ *
+ * @param {String} mediaType Media type to be converted into pandoc type identifier
+ * @return {String} Pandoc type identifier
+ */
+function convertType(mediaType) {
+    if (mediaTypeToPandoc[mediaType]) {
+        return mediaTypeToPandoc[mediaType];
+    } else if (mediaType.match(/^application\/x\./)) {
+        return mediaType.substr(mediaType.indexOf('.') + 1);
+    } else {
+        return mediaType;
+    }
 }
 
 module.exports = convertType;
